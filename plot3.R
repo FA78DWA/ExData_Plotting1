2f86bdc79eaed151fceb96dce73e168371bd66a8
Fadwa <fadwa.fawzy@gmail.com>
2017-03-21 13:24:13 +0100

    some minor changes

--- plot3.R
+++ plot3.R
@@ -4,6 +4,7 @@ power <- read.csv.sql("household_power_consumption.txt",
                       sql = "select * from file where Date == '1/2/2007' OR
                       Date == '2/2/2007' ", header = TRUE, sep = ";")
 
+png(filename = "plot3.png")
 plot(power$Sub_metering_1, type = "l",xlab = "", xaxt = "n", ylab = "Energy sub metering")
 lines(power$Sub_metering_2, type = "l", col = "red")
 lines(power$Sub_metering_3, type = "l", col = "blue")
@@ -11,12 +12,10 @@ lines(power$Sub_metering_3, type = "l", col = "blue")
 ## add legend
 leg.txt <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 leg.col <- c("black", "red", "blue")
-legend("topright", legend = leg.txt, col = leg.col, lty = 1, cex = 1, 
-       y.intersp = 0.5)
+legend("topright", legend = leg.txt, col = leg.col, lty = c(1,1,1))
 
 ## add x-axis label
 axis(1, at = seq(0,nrow(power), length.out = 3), labels = c("Thru","Fri","Sat"))
 
 ## save into png
-dev.copy(png, "plot3.png")
 dev.off()
